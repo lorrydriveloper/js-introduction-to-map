9748f502c598327179c8173674371e8dcdda25aa
reddevilcero <reddevil_cero@hotmail.com>
2020-07-10 15:00:15 +0100

    first test create

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code here
+
+function mapToNegative(array) {
+  let output = [];
+  for (const element of array) {
+    output.push(element * -1);
+  }
+  return output;
+}
--- test/index.test.js
+++ test/index.test.js
@@ -1,1 +1,10 @@
 // test here
+const baseArr = [1, 3, 5, 8, 9, -5];
+
+describe("my map-like functions", () => {
+  describe("mapToNegative", () => {
+    it("should transform in negative the original values", () => {
+      expect(mapToNegative(baseArr)).to.eql(baseArr.map((e) => e * -1));
+    });
+  });
+});
