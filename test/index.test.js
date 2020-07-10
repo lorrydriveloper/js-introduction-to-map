e0e701acc688498271f5c4bcc43190bc4f1b8846
reddevilcero <reddevil_cero@hotmail.com>
2020-07-10 15:38:13 +0100

    add reduce test

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,7 @@
 // test here
-const baseArr = [1, 3, 5, 8, 9, -5];
 
 describe("my map-like functions", () => {
+  const baseArr = [1, 3, 5, 8, 9, -5];
   describe("mapToNegative return an array with the original values made negative", () => {
     it("should transform in negative the original values", () => {
       expect(mapToNegative(baseArr)).to.eql(baseArr.map((e) => e * -1));
@@ -23,4 +23,25 @@ describe("my map-like functions", () => {
       expect(mapToSquare(baseArr)).to.eql(baseArr.map((e) => e * e));
     });
   });
+});
+
+describe("My reduce-like methods", () => {
+  const baseArr = [1, -5, true, 'CYF']
+
+  describe("reduceToTotal returns total from array when no stating point is given", () => {
+    it("should return the total sum of given array", () => {
+      expect(reduceToTotal(baseArr)).to.equal(
+        baseArr.reduce((memo, e) => e + memo)
+      );
+    });
+  });
+  describe("reduceToTotal returns total from array when stating point is given", () => {
+    it("should return the total sum of given array and starting point", () => {
+      const startingPoint = 100;
+      expect(reduceToTotal(baseArr, startingPoint)).to.equal(
+        baseArr.reduce((memo, e) => e + memo, startingPoint)
+      );
+    });
+  });
+
 });
